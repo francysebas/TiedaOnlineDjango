28b01453baff7de2eeadfad5a7f13d9176ee16b4
francysebas <31090790+francysebas@users.noreply.github.com>
2022-03-17 17:49:12 -0500

    api formularios y envio de email

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from gestionPedidos.models import Articulo
+from django.conf import settings
+from django.core.mail import send_mail
+from gestionPedidos.forms import FormularioContacto
+
 
 # Create your views here.
 
@@ -15,7 +19,7 @@ def buscar(request):
         # variable que guarda el producto del html
         producto = request.GET["producto"]
 
-        if len(producto)>20:
+        if len(producto) > 20:
             mensaje = "Texto de búsqueda demaciado largo"
 
         else:
@@ -28,7 +32,31 @@ def buscar(request):
 
     return HttpResponse(mensaje)
 
+
+'''
 def contacto(request):
     if request.method == "POST":
+        subject = request.POST["asunto"]
+        message = request.POST["mensaje"] + " " + request.POST["email"]
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ["francyn@unicauca.edu.co"]
+        send_mail(subject, message, email_from, recipient_list)
+
         return render(request, "gracias.html")
     return render(request, "contacto.html")
+'''
+
+
+def contacto(request):
+    if request.method == "POST":
+        miFormulario = FormularioContacto(request.POST)
+        if miFormulario.is_valid():
+            infForm = miFormulario.cleaned_data
+            send_mail(infForm['asunto'], infForm['mensaje'], infForm.get('email', ''), ['francyn@unicauca.edu.co'], )
+
+            return render(request, "gracias.html")
+
+    else:
+        miFormulario = FormularioContacto()
+
+    return render(request, "formulario_contacto.html", {"form": miFormulario})
